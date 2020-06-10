2a3d53723dedee9abd4f9e8bb28038ecde2769c3
PatrickPro2 <patrick_han_2@outlook.com>
2020-06-10 18:31:30 +0800

    add build

---
+++ build/precache-manifest.3ec2ef917eb1f795b2383796d36c8301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e28e801c40bdeb89f63228d0c31084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8515ffce527f3832165b",
+    "url": "/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "f6d1ab2368de754b2068",
+    "url": "/static/js/2.ae572ad4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ae572ad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8515ffce527f3832165b",
+    "url": "/static/js/main.b219d40c.chunk.js"
+  },
+  {
+    "revision": "eecfd3db2c3ee7d5d724",
+    "url": "/static/js/runtime-main.411587af.js"
+  }
+]);
